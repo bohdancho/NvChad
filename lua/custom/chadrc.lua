fac56562698d9eed73b95f5334abecbb58086568
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-12 20:24:14 +0200

    add lspconfig with tsserver + none-ls with stylua, eslint_d and prettierd

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,13 +1,8 @@
 ---@type ChadrcConfig
 local M = {}
 
--- M.plugins = "custom.plugins"
-
-M.ui = {
-  theme = "everblush",
-}
-
--- M.plugins = "custom.plugins"
+M.ui = { theme = "everblush" }
+M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
 -- M.mappings = require "custom.mappings"
