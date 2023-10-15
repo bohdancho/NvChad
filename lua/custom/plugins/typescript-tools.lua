903c429c159848ce6c373792f147e6b2ec8cb021
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-15 11:51:29 +0200

    expose typescript-tools actions as lsp code actions

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -2,5 +2,7 @@ return {
   "pmizio/typescript-tools.nvim",
   event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  opts = {
+    settings = { expose_as_code_action = "all" },
+  },
 }
