4290491ce87a57383b33625a2429ad8f0ec998e9
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-20 08:48:38 +0200

    replace typescript-tools with vtsls

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -3,14 +3,22 @@ return {
   dependencies = {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
+    "yioneko/nvim-vtsls",
   },
   config = function()
     local lspconfig = require "lspconfig"
+    require("lspconfig.configs").vtsls = require("vtsls").lspconfig -- set default server config, optional but recommended
+
     local config = require "plugins.configs.lspconfig"
     require("mason-lspconfig").setup {
-      ensure_installed = { "emmet_ls", "tailwindcss" },
+      ensure_installed = { "vtsls", "emmet_ls", "tailwindcss" },
     }
 
+    lspconfig.vtsls.setup {
+      on_attach = config.on_attach,
+      capabilities = config.capabilities,
+    }
+    --
     lspconfig.emmet_ls.setup {
       on_attach = config.on_attach,
       capabilities = config.capabilities,
--- lua/custom/plugins/typescript-tools.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "pmizio/typescript-tools.nvim",
-  event = "VeryLazy",
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {
-    settings = { expose_as_code_action = "all" },
-  },
-}
